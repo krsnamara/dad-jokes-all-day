7953ddb0d5915a45979fa997eafc583bb074d039
krsnamara <martinj.fitzpatrick@gmail.com>
2022-11-30 19:39:29 -0500

    markdown edited and update commit post class

---
+++ README.md
@@ -0,0 +1,26 @@
+# Quiz App
+
+## Description
+A simple web page that generates dad jokes to a user so that they can laugh a lot.
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript/jQuery
+- Rapid API Dad Jokes API
+- Git/Github - Github Pages for Hosting
+
+## Screenshots
+![wireframe]()
+![production version]()
+
+## Getting started
+[click here](#) to visit deployed app!
+
+## Future Enhancements
+- Adding a feature that allows users to create accounts to save data
+- Adding a feature that allows a user to score points when they answer quiz
+- Adding a feature that allows persistence of high scores
+- Adding a feature for users to select topic specific quiz questions
+
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,56 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://dad-jokes.p.rapidapi.com/joke/type/general",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "5e13500d5bmsh9a68ff011401beep10e215jsnc7be98aadae1",
-		"X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
-	}
-};
+////// TESTING JS AND jQuery
+// alert('js is working');
+// console.log('jQuerry', $);
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+/// IIFE----> imediatly invoked function expression
+/// add at the end
+//$(function() {
+
+    /// IPO - Input ---> Process ---> Output --->
+
+    /// Constants and Variables
+
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://dad-jokes.p.rapidapi.com/random/joke/png",
+        "method": "GET",
+        "headers": {
+            "X-RapidAPI-Key": "67fdb2c8f5msh07c8daabd1899a4p11e21djsnf044be486fc5",
+            "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    
+
+    /// Cache DOM element references
+
+    /// Register Event Listners
+   
+    /// Functions
+
+
+    /// manually invoke get data
+ 
+    /// seprating concerns by putting this function here instead of in the dom above
+
+
+    /// Psuedocoe
+
+    /*
+    1) select a dom element I can use to insert some quiz cards into
+
+    2) make an ajax request to the api and get some quiz data
+
+    3) loop over the list of quiz question objects and generate an html card for each object
+
+    4) use the selected dom element to append my quiz cards to the dom
+
+    */
+//})
+
+
+
