a964296de8a67c2ec149d7e712b0fdb5da3188e4
krsnamara <martinj.fitzpatrick@gmail.com>
2022-12-02 17:28:55 -0500

    updating the functions to log static data vs api

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,16 @@
 
     /// Constants and Variables
 
-    const API_URL = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://dad-jokes.p.rapidapi.com/random/joke/png",
-        "method": "GET",
-        "headers": {
-            "X-RapidAPI-Key": "67fdb2c8f5msh07c8daabd1899a4p11e21djsnf044be486fc5",
-            "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
-        }
-    };
+    // const API_URL = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://dad-jokes.p.rapidapi.com/random/joke/png",
+    //     "method": "GET",
+    //     "headers": {
+    //         "X-RapidAPI-Key": "67fdb2c8f5msh07c8daabd1899a4p11e21djsnf044be486fc5",
+    //         "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+    //     }
+    // };
 
 
     //use to generate api object to grab values
@@ -49,18 +49,13 @@
     /// Functions
 
     function getJoke() {
-        $.ajax(API_URL)
-        .then(( data ) => {
-            joke = data
-            $jokeSection.text(data.body.setup);
-        }, function(error) {
-            console.log(error);
-        });
+            $jokeSection.appendto('What is so great about a boolean?')
+            );
     }
 
     // second on click doesnt need a second api call 
     function getPunchline() {
-        $punchlineSection.text(joke.body.punchline);
+        $punchlineSection.apendto("if you use it wrong you're only off by a little bit");
         }
 
     /// Psuedocoe
